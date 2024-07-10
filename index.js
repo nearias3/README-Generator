04f11b472e1b76d6d4a2c0b8cab590fa18707149
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-07-09 21:18:38 -0500

    Added all the questions I want the user to be prompted

--- index.js
+++ index.js
@@ -4,7 +4,44 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+},
+
+{
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+},
+
+{
+    type: 'input',
+    name: 'Usage',
+    message: 'Provide the link to your deployed application:'
+},
+
+{
+    type: 'input',
+    name: 'screenshot',
+    message: 'Provide the path to the screenshot for your deployed application:',
+},
+
+{
+    type: 'input',
+    name: 'Credits',
+    message: 'What sources did you use for your application?',
+},
+
+{
+    type: 'input',
+    name: 'License',
+    message: 'What license do you want to use?',
+},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
